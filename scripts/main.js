452278eb35e0ef71f237f1749c3c32bac4ba3edf
revilowaldow <52298102+revilowaldow@users.noreply.github.com>
2021-12-12 11:53:40 +0000

    Files

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+Hooks.once('diceSoNiceInit', (dice3d) => {
+    dice3d.addTexture("AngryFan", {
+        name: "AngryFan",
+        composite: "source-over",
+        source: "modules/angryDiceFan/images/fan.png",
+        bump:"modules/angryDiceFan/images/fan.png"
+    });
+
+});
